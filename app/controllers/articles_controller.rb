e3ef20e4a967df3441dc1734d764dcdf7417eb5a
lutfor3737 <mdlutforrahman@csebuet.org>
2016-01-23 19:44:31 -0600

    extract redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+
+	before_action :set_article, only: [:edit,:update, :show, :destroy] 
 	
 	def index
 		@articles = Article.all
@@ -21,7 +23,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully updated"
 			redirect_to article_path(@article)
@@ -32,21 +33,22 @@ class ArticlesController < ApplicationController
 
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def show
-		@article = Article.find(params[:id])
+
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 		flash[:notice] = "Article was successfully deleted"
 		redirect_to articles_path
 	end
 
 	private 
+		def set_article
+			@article = Article.find(params[:id])
+		end
 		def article_params
 			params.require(:article).permit(:title, :description)
 		end
