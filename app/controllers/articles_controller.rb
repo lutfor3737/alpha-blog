833b055250eda3cbdeef3ca31ef5c7886e34e210
lutfor3737 <mdlutforrahman@csebuet.org>
2016-01-25 16:46:32 -0600

    some coding change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,7 @@ class ArticlesController < ApplicationController
 
 	def update
 		if @article.update(article_params)
+		    @article.user = User.first
 			flash[:success] = "Article was successfully updated"
 			redirect_to article_path(@article)
 		else
